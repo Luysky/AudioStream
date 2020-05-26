56ecd669a3fee078c44f266460be69bd4226a4c0
Marshasha <mspieldenner@gmx.de>
2020-05-26 16:01:10 +0200

    Music !!!

--- src/HES/MainClientOne.java
+++ src/HES/MainClientOne.java
@@ -7,6 +7,7 @@ public class MainClientOne {
     public static void main(String[] args) throws IOException, InterruptedException {
 
         ClientOne clientOne = new ClientOne();
+        System.out.println("Salut");
 
     }
 }
